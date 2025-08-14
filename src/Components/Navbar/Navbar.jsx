82e60c67ae9c55a16eef4c4f09aad55831ca4bb7
Argeem <sitthisakbannob@gmail.com>
2025-08-15 00:15:32 +0700

    Add navbar menu , install font and initial route page

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,24 +1,53 @@
-import React from "react";
 import "./Navbar.css";
 import logo from "../Assets/logo.png";
 import cart_icon from "../Assets/cart_icon.png";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export const Navbar = () => {
+  const [menu, setMenu] = useState("shop");
+
+  const allMenu = [
+    { stage: "shop", stageLabel: "Shop", link: "/" },
+    { stage: "mens", stageLabel: "Men", link: "/mens" },
+    { stage: "womens", stageLabel: "Women", link: "/womens" },
+    { stage: "kids", stageLabel: "Kids", link: "/kids" },
+  ];
+
+  const getMenu = (stage, stageLabel, link) => {
+    return (
+      <li
+        onClick={() => {
+          setMenu(stage);
+        }}
+      >
+        <Link to={link} className="nav-link">
+          {stageLabel}
+        </Link>
+        {menu === stage ? <hr /> : <></>}
+      </li>
+    );
+  };
+
   return (
     <div className="navbar">
       <div className="nav-logo">
         <img src={logo} alt="logo" />
         <p>SHOPPER</p>
-        <ul className="nav-menu">
-          <li>Shop</li>
-          <li>Men</li>
-          <li>Women</li>
-          <li>Kids</li>
-        </ul>
-        <div className="nav-login-cart">
+      </div>
+      <ul className="nav-menu">
+        {allMenu.map((item) => {
+          return getMenu(item.stage, item.stageLabel, item.link);
+        })}
+      </ul>
+      <div className="nav-login-cart">
+        <Link to="/login">
           <button>Login</button>
+        </Link>
+        <Link to="/cart">
           <img src={cart_icon} alt="cart_icon" />
-        </div>
+        </Link>
+        <div className="nav-cart-count">0</div>
       </div>
     </div>
   );
