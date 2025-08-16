5eb22f4ad6c3c37468b46b5df48d87b212b1942e
Argeem <sitthisakbannob@gmail.com>
2025-08-16 17:05:58 +0700

    Add new potion on main shop include new collection, news letter, offers and footer

--- src/Pages/Shop.jsx
+++ src/Pages/Shop.jsx
@@ -1,11 +1,17 @@
 import { Hero } from "../Components/Hero/Hero";
 import { Popular } from "../Components/Popular/Popular";
+import { Offers } from "../Components/Offers/Offers";
+import { NewCollection } from "../Components/NewCollection/NewCollection";
+import { NewsLetter } from "../Components/NewsLetter/NewsLetter";
 
 export const Shop = () => {
   return (
     <div>
       <Hero />
       <Popular />
+      <Offers />
+      <NewCollection />
+      <NewsLetter />
     </div>
   );
 };
