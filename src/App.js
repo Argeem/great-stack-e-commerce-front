c93915541014de4ac9ede2a2e0aec97a4e29e025
Argeem <sitthisakbannob@gmail.com>
2025-08-16 23:16:14 +0700

    Create all product context and create category page

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import { Product } from "./Pages/Product";
 import { Cart } from "./Pages/Cart";
 import { LoginSignup } from "./Pages/LoginSignup";
 import { Footer } from "./Components/Footer/Footer";
+import man_banner from "./Components/Assets/banner_mens.png";
+import women_banner from "./Components/Assets/banner_women.png";
+import kid_banner from "./Components/Assets/banner_kids.png";
 
 function App() {
   return (
@@ -14,9 +17,18 @@ function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Shop />} />
-          <Route path="/mens" element={<ShopCategory category="mens" />} />
-          <Route path="/womens" element={<ShopCategory category="womens" />} />
-          <Route path="/kids" element={<ShopCategory category="kids" />} />
+          <Route
+            path="/mens"
+            element={<ShopCategory category="men" banner={man_banner} />}
+          />
+          <Route
+            path="/womens"
+            element={<ShopCategory category="women" banner={women_banner} />}
+          />
+          <Route
+            path="/kids"
+            element={<ShopCategory category="kid" banner={kid_banner} />}
+          />
           <Route path="/product" element={<Product />}>
             <Route path=":productId" element={<Product />} />
           </Route>
