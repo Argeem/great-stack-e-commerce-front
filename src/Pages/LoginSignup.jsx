b19eb562ecf6fecaab7b3d639e74829a73fed165
Argeem <sitthisakbannob@gmail.com>
2025-08-17 18:35:45 +0700

    Add login signup Page

--- src/Pages/LoginSignup.jsx
+++ src/Pages/LoginSignup.jsx
@@ -1,3 +1,24 @@
+import "./Css/LoginSignup.css";
+
 export const LoginSignup = () => {
-  return <div>LoginSignup</div>;
+  return (
+    <div className="loginSignup">
+      <div className="loginsignup-container">
+        <h1>Sign Up</h1>
+        <div className="loginsignup-fields">
+          <input type="text" placeholder="Your Name" />
+          <input type="email" placeholder="Email Address" />
+          <input type="password" placeholder="Password" />
+        </div>
+        <button>Continue</button>
+        <p className="loginsignup-login">
+          Already have an account? <span>Login here</span>
+        </p>
+        <div className="loginsignup-agree">
+          <input type="checkbox" name="" id="" />
+          <p>To continueing, i agree to the terms of use & privacy policy.</p>
+        </div>
+      </div>
+    </div>
+  );
 };
